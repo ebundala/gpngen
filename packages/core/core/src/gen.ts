5ebcc337f865cf82cd7a7f7136fe0963b55a6c00
Elias Bundala <ebundala@gmail.com>
2021-02-26 04:27:28 +0300

    chore(monorepo) configure monorepo project

--- packages/core/core/src/gen.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { GenModule } from './modules/sdl-generator/gen.module';
-
-async function bootstrap() {
-    const app = await NestFactory.createApplicationContext(GenModule);
-    await app.init()
-}
-bootstrap();
--- packages/core/core/src/modules/casbin-admin/casbin-admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CasbinAdminService } from './casbin-admin.service';
-
-@Module({
-  //  imports: [CasbinModule],
-  providers: [CasbinAdminService],
-  // controllers: [CasbinAdminController]
-})
-export class CasbinAdminModule { }
