5bbb0f6d6d6f9b5d3d4624aace6626a9d48b6f1e
Elias Bundala <ebundala@gmail.com>
2021-07-13 20:03:30 +0300

    ci

--- packages/console/src/main.policy.ts
+++ packages/console/src/main.policy.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppAuthModule } from './app.auth.module'
 export async function bootstrap2() {
     const app2 = await NestFactory.create(AppAuthModule)
-    await app2.listen(4000)
+    await app2.listen(4000);
+    
 
 }
 
