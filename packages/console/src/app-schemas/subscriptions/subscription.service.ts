84a727a298b78e8bbe2d58d5a165ddc46acbff4b
Elias Bundala <ebundala@gmail.com>
2021-08-15 02:34:11 +0300

    feature(fcm) complete notifications

--- packages/console/src/app-schemas/subscriptions/subscription.service.ts
+++ packages/console/src/app-schemas/subscriptions/subscription.service.ts
@@ -1,11 +1,12 @@
 import { Bloc, PrismaAttach, PrismaHookHandler, PrismaHookRequest } from "@mechsoft/business-rules-manager";
 import { Inject, Injectable } from "@nestjs/common";
 import { RedisPubSub } from "graphql-redis-subscriptions"
-import { Invite, Order, Rating,Location, LatLon, NotificationType,Notification } from "src/models/graphql";
+import { Invite, Order, Rating, Location, LatLon, NotificationType, Notification } from "src/models/graphql";
 import { RedisCache } from "src/pubsub/redis.service";
 import { FirebaseService } from "@mechsoft/firebase-admin";
-import { PrismaClient} from '@mechsoft/prisma-client';
+import { PrismaClient } from '@mechsoft/prisma-client';
 import { State } from "@prisma/client";
+import { AppLogger } from "@mechsoft/app-logger";
 
 export const ORDER_CHANGED = "ORDER_CHANGED";
 export const ORDER_RECEIVED = "ORDER_RECEIVED";
@@ -16,214 +17,215 @@ export const LOCATION_CHANGED = "LOCATION_CHANGED";
 @Injectable()
 @Bloc()
 export class SubscriptionService {
-
   constructor(
     private readonly pubSub: RedisPubSub,
-    private readonly redisCache:RedisCache,
+    private readonly redisCache: RedisCache,
     private readonly client: PrismaClient,
-    private readonly app: FirebaseService) {
+    private readonly app: FirebaseService,
+    private readonly log: AppLogger) {
     //register handlers for fcm here
-    this.pubSub.subscribe(ORDER_RECEIVED,this.orderNotifications.bind(this));
-    this.pubSub.subscribe(ORDER_CHANGED,this.orderChangeNotifications.bind(this));
-    this.pubSub.subscribe(INVITE_RECEIVED,this.inviteNotifications.bind(this));
-    this.pubSub.subscribe(FEEDBACK_RECEIVED,this.reviewNotifications.bind(this));
+    this.pubSub.subscribe(ORDER_RECEIVED, this.orderNotifications.bind(this));
+    this.pubSub.subscribe(ORDER_CHANGED, this.orderChangeNotifications.bind(this));
+    this.pubSub.subscribe(INVITE_RECEIVED, this.inviteNotifications.bind(this));
+    this.pubSub.subscribe(FEEDBACK_RECEIVED, this.reviewNotifications.bind(this));
   }
 
-  async orderNotifications(data:Order){
-    
-      const order= this.client.rating.findUnique({where:{id:data.id}})  
+  async orderNotifications(data: Order) {
+    debugger
+    const manager = await this.client.order.findUnique({ where: { id: data.id } })
+      .organization().owner().device({ select: { fcm_id: true } });
 
-      const manager = await order.organization().owner().device({select:{fcm_id:true}});
-     const message:Notification = {
-        notificationType:NotificationType.ORDER_RECIEVED,
-        message: "You have new order",
-        payload: data
-      };
-      if(manager?.fcm_id)
-      await this.app.sendNotification(manager.fcm_id,{
-        notification:{
-          title:message.notificationType.replace("_", " "),     
+    const message: Notification = {
+      notificationType: NotificationType.ORDER_RECIEVED,
+      message: "You have new order",
+      payload: data
+    };
+    if (manager?.fcm_id) {
+      const result = await this.app.sendNotification(manager.fcm_id, {
+        notification: {
+          title: message.notificationType.replace("_", " "),
         },
         data: {
-          payload:JSON.stringify(message)
+          payload: JSON.stringify(message)
         }
-      },{
-        priority:"high"
-      }).catch(e=>e);
+      }, {
+        priority: "high"
+      }).catch(e => e);
+      this.log.debug(result);
+    }
   }
-   async orderChangeNotifications(data:{original:Order,changed:Order}){
-      debugger
-      const {original,changed,} = data;
-      const order= this.client.order.findUnique({where:{id:changed.id}})  
+  async orderChangeNotifications(data: { original: Order, changed: Order }) {
+    debugger
+    const { original, changed, } = data;
+    const order = this.client.order.findUnique({ where: { id: changed.id } })
 
-      const owner = await order.owner().device({select:{fcm_id:true}});
-      const provider = await order.provider().device({select:{fcm_id:true}});
-      const manager = await order.organization().owner().device({select:{fcm_id:true}});
-      console.log(owner,provider,manager);
-      let message:Notification;
-      const devices = new Set<string>();
-      if(original.state!==changed.state){
-        //order changed state
+    const owner = await order.owner().device({ select: { fcm_id: true } });
+    const provider = await order.provider().device({ select: { fcm_id: true } });
+    const manager = await order.organization().owner().device({ select: { fcm_id: true } });
+    this.log.debug(owner, provider, manager);
+    let message: Notification;
+    const devices = new Set<string>();
+    if (original.state !== changed.state) {
+      //order changed state
+      message = {
+        notificationType: NotificationType.ORDER_ACCEPTED,
+        message: "Order state changed",
+        payload: changed
+      };
+      //audience depends on new state
+      // review => user
+      // approved => user
+      if (changed.state == State.REVIEW) {
+        message.notificationType = NotificationType.ORDER_ACCEPTED
+        if (owner?.fcm_id)
+          devices.add(owner.fcm_id)
+      }
+      if (changed.state == State.APPROVED) {
+        message.notificationType = NotificationType.ORDER_DISPATCHED
+        if (owner?.fcm_id)
+          devices.add(owner.fcm_id)
+      }
+      // completed => user/owner
+      if (changed.state == State.COMPLETED) {
+        message.notificationType = NotificationType.ORDER_DELIVERED;
+
+        ([manager?.fcm_id, owner?.fcm_id]).forEach(fcm_id => { devices.add(fcm_id) });
+      }
+
+      // archived  => user/provider/owner
+      // cancelled => all
+      if (changed.state == State.REJECTED) {
+
+        message.notificationType = NotificationType.ORDER_CANCELLED;
+
+        ([owner?.fcm_id, manager?.fcm_id, provider?.fcm_id]).forEach(fcm_id => { devices.add(fcm_id) });
+      }
+      if (changed.state == State.ARCHIVED) {
+        message.notificationType = NotificationType.ORDER_PAYED;
+        ([owner?.fcm_id, manager?.fcm_id, provider?.fcm_id]).forEach(fcm_id => { devices.add(fcm_id) });
+      }
+    }
+    else if (original.quantity !== changed.quantity
+      || original.notes !== changed.notes
+      || original.location?.lat != changed.location?.lat
+      || original.location?.lon != changed.location?.lon
+    ) {
+      message = {
+        notificationType: NotificationType.ORDER_UPDATED,
+        message: "Order state changed",
+        payload: changed
+      };
+      //the order was updated
+      //Pending => owner
+      if (changed.state == State.PENDING) {
         message = {
-          notificationType:NotificationType.ORDER_ACCEPTED,
+          notificationType: NotificationType.ORDER_UPDATED,
           message: "Order state changed",
           payload: changed
         };
-        //audience depends on new state
-        // review => user
-        // approved => user
-        if(changed.state == State.REVIEW){
-          message.notificationType=NotificationType.ORDER_ACCEPTED
-          if(owner?.fcm_id)
-          devices.add(owner.fcm_id)
-        }
-        if(changed.state == State.APPROVED ){
-          message.notificationType=NotificationType.ORDER_DISPATCHED
-          if(owner?.fcm_id)
-          devices.add(owner.fcm_id)
-        }
-        // completed => user/owner
-        if(changed.state == State.COMPLETED){
-          message.notificationType=NotificationType.ORDER_DELIVERED
-         
-        [manager?.fcm_id,owner?.fcm_id].forEach(fcm_id=>{devices.add(fcm_id)});
-        }
-
-        // archived  => user/provider/owner
-        // cancelled => all
-        if(changed.state == State.REJECTED){
-          
-          message.notificationType=NotificationType.ORDER_CANCELLED
-
-          [owner?.fcm_id,manager?.fcm_id,provider?.fcm_id].forEach(fcm_id=>{devices.add(fcm_id)});
-        } 
-        if(changed.state == State.ARCHIVED){
-          message.notificationType=NotificationType.ORDER_PAYED
-
-          [owner?.fcm_id,manager?.fcm_id,provider?.fcm_id].forEach(fcm_id=>{devices.add(fcm_id)});
-        } 
-      }
-      else if(original.quantity!==changed.quantity 
-        || original.notes!==changed.notes
-        || original.location?.lat!=changed.location?.lat
-        || original.location?.lon!=changed.location?.lon
-        ){
-          message = {
-            notificationType:NotificationType.ORDER_UPDATED,
-            message: "Order state changed",
-            payload: changed
-          };
-        //the order was updated
-        //Pending => owner
-        if(changed.state == State.PENDING ){
-          message = {
-            notificationType:NotificationType.ORDER_UPDATED,
-            message: "Order state changed",
-            payload: changed
-          };
-          if(manager?.fcm_id)
+        if (manager?.fcm_id)
           devices.add(manager.fcm_id)
-        }
-       //Review => manager/provider
-       if(changed.state == State.REVIEW || changed.state == State.APPROVED ){
+      }
+      //Review => manager/provider
+      if (changed.state == State.REVIEW || changed.state == State.APPROVED) {
         message = {
-          notificationType:NotificationType.ORDER_UPDATED,
+          notificationType: NotificationType.ORDER_UPDATED,
           message: "Order state changed",
           payload: changed
         };
-        [provider?.fcm_id,manager?.fcm_id].forEach(fcm_id=>{devices.add(fcm_id)});
+        ([provider?.fcm_id, manager?.fcm_id]).forEach(fcm_id => { devices.add(fcm_id) });
       }
-        }
-
-      //todo send to fcm
-      
-      if(devices.size>0){
-        const filtered = [];
-        devices.forEach(fcm_id=>{
-          if(fcm_id)filtered.push(fcm_id);});
-    let result =  await this.app.sendNotification(filtered,{
-       notification:{
-         title:message.notificationType.replace("_", " "),     
-       },
-        data: {
-          payload:JSON.stringify(message)
-        }
-      },{
-        priority:"high"
-      }).catch(e=>e);
-      console.log(result);
-    }
     }
 
-  async inviteNotifications(data:{result:Invite}){
-    const {result} = data;
-      const order= this.client.invite.findUnique({where:{id:result.id}})  
+    //todo send to fcm
 
-      const invitee = await order.invitee().device({select:{fcm_id:true}});
-     const message:Notification = {
-        notificationType:NotificationType.INVITE_RECIEVED,
-        message: "You have been invited to join an organization",
-        payload: result
-      };
-      if(invitee?.fcm_id)
-      await this.app.sendNotification(invitee.fcm_id,{
-        notification:{
-          title:message.notificationType.replace("_", " "),     
+    if (devices.size > 0) {
+      const filtered = [];
+      devices.forEach(fcm_id => {
+        if (fcm_id) filtered.push(fcm_id);
+      });
+      this.log.debug(filtered);
+      let result = await this.app.sendNotification(filtered, {
+        notification: {
+          title: message.notificationType.replace("_", " "),
         },
         data: {
-          payload:JSON.stringify(message)
+          payload: JSON.stringify(message)
         }
-      },{
-        priority:"high"
-      }).catch(e=>e);
+      }, {
+        priority: "high"
+      }).catch(e => e);
+      this.log.debug(result);
+    }
   }
 
-  async reviewNotifications(data:{result:Rating}){
-    const {result} = data;
-      const order= this.client.rating.findUnique({where:{id:result.id}})  
+  async inviteNotifications(data: { result: Invite }) {
+    const { result } = data;
+    const order = this.client.invite.findUnique({ where: { id: result.id } })
 
-      const reviewee = await order.owner().device({select:{fcm_id:true}});
-     const message:Notification = {
-        notificationType:NotificationType.REVIEW_RECIEVED,
-        message: "You have new review",
-        payload: result
-      };
-      if(reviewee?.fcm_id)
-      await this.app.sendNotification(reviewee.fcm_id,{
-        notification:{
-          title:message.notificationType.replace("_", " "),     
+    const invitee = await order.invitee().device({ select: { fcm_id: true } });
+    const message: Notification = {
+      notificationType: NotificationType.INVITE_RECIEVED,
+      message: "You have been invited to join an organization",
+      payload: result
+    };
+    if (invitee?.fcm_id)
+      await this.app.sendNotification(invitee.fcm_id, {
+        notification: {
+          title: message.notificationType.replace("_", " "),
         },
         data: {
-          payload:JSON.stringify(message)
+          payload: JSON.stringify(message)
         }
-      },{
-        priority:"high"
-      }).catch(e=>e);
+      }, {
+        priority: "high"
+      }).catch(e => e);
   }
-@PrismaAttach("Order", "create")
+
+  async reviewNotifications(data: { result: Rating }) {
+    const { result } = data;
+    const order = this.client.rating.findUnique({ where: { id: result.id } })
+
+    const reviewee = await order.owner().device({ select: { fcm_id: true } });
+    const message: Notification = {
+      notificationType: NotificationType.REVIEW_RECIEVED,
+      message: "You have new review",
+      payload: result
+    };
+    if (reviewee?.fcm_id)
+      await this.app.sendNotification(reviewee.fcm_id, {
+        notification: {
+          title: message.notificationType.replace("_", " "),
+        },
+        data: {
+          payload: JSON.stringify(message)
+        }
+      }, {
+        priority: "high"
+      }).catch(e => e);
+  }
+  @PrismaAttach("Order", "create")
   async orderCreated(req: PrismaHookRequest<Order>, n: PrismaHookHandler) {
-    const { result ,prisma,params} = req;
-   // const {action,args} = params;
-    
-    await this.pubSub.publish(`${ORDER_RECEIVED}`, result )
+    const { result, prisma, params } = req;
+    // const {action,args} = params;
+    this.pubSub.publish(`${ORDER_RECEIVED}`, result)
     return n(req);
   }
 
-  @PrismaAttach("Order", "update",true)
+  @PrismaAttach("Order", "update", true)
   async orderPreUpdated(req: PrismaHookRequest<Order>, n: PrismaHookHandler) {
-    const { result ,prisma,params} = req;
-    const {action,args} = params;
-    
+    const { result, prisma, params } = req;
+    const { action, args } = params;
+
     //record its state before updating
-    req.context= await prisma.order.findUnique({where:args.where,select:args.select});
-    //await this.pubSub.publish(`${ORDER_CHANGED}`, { id: result.id })
+    req.context = await prisma.order.findUnique({ where: args.where, select: args.select });
     return n(req);
   }
   @PrismaAttach("Order", "update")
   async orderHasbeenUpdated(args: PrismaHookRequest<Order>, n: PrismaHookHandler) {
-    const { result,context } = args;
+    const { result, context } = args;
     //construct messages and publish to notifications pubsub
-    this.pubSub.publish(`${ORDER_CHANGED}`, { id: result.id ,original:context,changed:result });
+    this.pubSub.publish(`${ORDER_CHANGED}`, { id: result.id, original: context, changed: result });
     return n(args);
   }
 
@@ -231,7 +233,7 @@ export class SubscriptionService {
   async inviteReceived(args: PrismaHookRequest<Invite>, n: PrismaHookHandler) {
     const { result } = args;
 
-    this.pubSub.publish(`${INVITE_RECEIVED}`, { id: result.id ,result})
+    this.pubSub.publish(`${INVITE_RECEIVED}`, { id: result.id, result })
     return n(args);
   }
 
@@ -240,21 +242,21 @@ export class SubscriptionService {
   async feedbackReceived(args: PrismaHookRequest<Rating>, n: PrismaHookHandler) {
     const { result } = args;
 
-    this.pubSub.publish(`${FEEDBACK_RECEIVED}`, { id: result.id ,result})
+    this.pubSub.publish(`${FEEDBACK_RECEIVED}`, { id: result.id, result })
     return n(args);
   }
 
-  async updateUserLocation(uid: string,location:Location ){
-    if(uid&&location&&location.lat&&location.lon){
+  async updateUserLocation(uid: string, location: Location) {
+    if (uid && location && location.lat && location.lon) {
       const key = `location/${uid}`;
-     await this.redisCache.set(key,JSON.stringify(location));
-     this.pubSub.publish(LOCATION_CHANGED,{id:uid});
+      await this.redisCache.set(key, JSON.stringify(location));
+      this.pubSub.publish(LOCATION_CHANGED, { id: uid });
     }
-    
+
   }
-  async getUserLocation(uid:string): Promise<LatLon>{
+  async getUserLocation(uid: string): Promise<LatLon> {
     const key = `location/${uid}`;
-    const v=await this.redisCache.get(key);
-    if(v) return JSON.parse(v);
+    const v = await this.redisCache.get(key);
+    if (v) return JSON.parse(v);
   }
 }
