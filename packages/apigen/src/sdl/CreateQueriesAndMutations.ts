1f2d246cdbac94bcebb39fad623e622577bd705e
Elias Bundala <ebundala@gmail.com>
2022-05-04 12:20:49 +0300

    fix(operations): Fix empty operation generation

--- packages/apigen/src/sdl/CreateQueriesAndMutations.ts
+++ packages/apigen/src/sdl/CreateQueriesAndMutations.ts
@@ -8,294 +8,287 @@ export function createQueriesAndMutations(
 ): any {
   const operations = {
     queries: {
-      type: 'extend type Query {',
-      resolver: '',
+        type: '',
+        resolver: '',
     },
     mutations: {
-      type: '\nextend type Mutation {',
-      resolver: '',
+        type: '',
+        resolver: '',
     },
-  };
-
-  if (!exclude.includes('findUnique')) {
-    operations.queries.type += `
-    findUnique${name}(where: ${name}WhereUniqueInput!): ${name}Response!`;
+};
+let queries='';
+let mutations='';
+if (!exclude.includes('findUnique')) {
+   queries += `
+findUnique${name}(where: ${name}WhereUniqueInput!): ${name}Response!`;
     operations.queries.resolver += `
-    @Query((returns)=>${name}Response)
+@Query((returns)=>${name}Response)
 
-    findUnique${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.findUnique({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('findFirst')) {
-    operations.queries.type += `
-    findFirst${name}(
-      where: ${name}WhereInput
-      orderBy: [${name}OrderByInput!]
-      cursor: ${name}WhereUniqueInput
-      distinct: [${name}ScalarFieldEnum!]
-      skip: Int
-      take: Int
-    ): ${name}Response!`;
+findUnique${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.findUnique({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('findFirst')) {
+   queries += `
+findFirst${name}(
+  where: ${name}WhereInput
+  orderBy: [${name}OrderByInput!]
+  cursor: ${name}WhereUniqueInput
+  distinct: [${name}ScalarFieldEnum!]
+  skip: Int
+  take: Int
+): ${name}Response!`;
     operations.queries.resolver += `
-    @Query((returns)=>${name}Response)
-    findFirst${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.findFirst({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:[],
-       message:message||'unknown error'
-      }));
-      
-    }`;
-  }
-
-  if (!exclude.includes('findMany')) {
-    operations.queries.type += `
-    findMany${name}(
-      where: ${name}WhereInput
-      orderBy: [${name}OrderByInput!]
-      cursor: ${name}WhereUniqueInput
-      distinct: [${name}ScalarFieldEnum!]
-      skip: Int
-      take: Int
-    ): ${name}ListResponse!`;
+@Query((returns)=>${name}Response)
+findFirst${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.findFirst({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:[],
+   message:message||'unknown error'
+  }));
+  
+}`;
+}
+if (!exclude.includes('findMany')) {
+   queries += `
+findMany${name}(
+  where: ${name}WhereInput
+  orderBy: [${name}OrderByInput!]
+  cursor: ${name}WhereUniqueInput
+  distinct: [${name}ScalarFieldEnum!]
+  skip: Int
+  take: Int
+): ${name}ListResponse!`;
     operations.queries.resolver += `
-    @Query((returns)=>${name}ListResponse)
-    findMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}ListResponse|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.findMany({...args,...select})
-      .then((data)=>({
-        status:true,
-        data:data??[],
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:[],
-       message:message||'unknown error'
-      }));
-      
-    }`;
-  }
-
-  if (!exclude.includes('findCount')) {
-    operations.queries.type += `
-    findMany${name}Count(
-      where: ${name}WhereInput
-      orderBy: [${name}OrderByInput!]
-      cursor: ${name}WhereUniqueInput
-      distinct: [${name}ScalarFieldEnum!]
-      skip: Int
-      take: Int
-    ): ${name}CountResponse!`;
+@Query((returns)=>${name}ListResponse)
+findMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}ListResponse|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.findMany({...args,...select})
+  .then((data)=>({
+    status:true,
+    data:data??[],
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:[],
+   message:message||'unknown error'
+  }));
+  
+}`;
+}
+if (!exclude.includes('findCount')) {
+   queries += `
+findMany${name}Count(
+  where: ${name}WhereInput
+  orderBy: [${name}OrderByInput!]
+  cursor: ${name}WhereUniqueInput
+  distinct: [${name}ScalarFieldEnum!]
+  skip: Int
+  take: Int
+): ${name}CountResponse!`;
     operations.queries.resolver += `
-    @Query((returns)=>${name}CountResponse)
-    findMany${name}Count(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}CountResponse|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.count({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('aggregate')) {
-    operations.queries.type += `
-    aggregate${name}(
-      where: ${name}WhereInput
-      orderBy: [${name}OrderByInput!]
-      cursor: ${name}WhereUniqueInput
-      distinct: [${name}ScalarFieldEnum!]
-      skip: Int
-      take: Int
-    ): Aggregate${name}Response!`;
+@Query((returns)=>${name}CountResponse)
+findMany${name}Count(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}CountResponse|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.count({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('aggregate')) {
+   queries += `
+aggregate${name}(
+  where: ${name}WhereInput
+  orderBy: [${name}OrderByInput!]
+  cursor: ${name}WhereUniqueInput
+  distinct: [${name}ScalarFieldEnum!]
+  skip: Int
+  take: Int
+): Aggregate${name}Response!`;
     operations.queries.resolver += `
-    @Query((returns)=>Aggregate${name}Response)
-    aggregate${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<Aggregate${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+@Query((returns)=>Aggregate${name}Response)
+aggregate${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<Aggregate${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
 
-      return ctx.prisma.${model}.aggregate({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('createOne')) {
-    operations.mutations.type += `
-    createOne${name}(data: ${name}CreateInput!): ${name}Response!`;
+  return ctx.prisma.${model}.aggregate({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('createOne')) {
+    mutations += `
+createOne${name}(data: ${name}CreateInput!): ${name}Response!`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}Response)
-    createOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.create({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('updateOne')) {
-    operations.mutations.type += `
-    updateOne${name}(
-      where: ${name}WhereUniqueInput!
-      data: ${name}UpdateInput!
-    ): ${name}Response!`;
+@Mutation((returns)=>${name}Response)
+createOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.create({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('updateOne')) {
+    mutations += `
+updateOne${name}(
+  where: ${name}WhereUniqueInput!
+  data: ${name}UpdateInput!
+): ${name}Response!`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}Response)
-    updateOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.update({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('deleteOne')) {
-    operations.mutations.type += `
-    deleteOne${name}(where: ${name}WhereUniqueInput!): ${name}Response!`;
+@Mutation((returns)=>${name}Response)
+updateOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.update({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('deleteOne')) {
+    mutations += `
+deleteOne${name}(where: ${name}WhereUniqueInput!): ${name}Response!`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}Response)
-    async deleteOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      ${onDelete
+@Mutation((returns)=>${name}Response)
+async deleteOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  ${onDelete
         ? `await ctx.prisma.onDelete({ model: '${name}', where: args.where })`
-        : ''
-      }
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+        : ''}
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
 
-      return ctx.prisma.${model}.delete({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }))
-    }`;
-  }
-
-  if (!exclude.includes('upsertOne')) {
-    operations.mutations.type += `
-    upsertOne${name}(
-      where: ${name}WhereUniqueInput!
-      create: ${name}CreateInput!
-      update: ${name}UpdateInput!
-    ): ${name}Response!`;
+  return ctx.prisma.${model}.delete({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }))
+}`;
+}
+if (!exclude.includes('upsertOne')) {
+    mutations += `
+upsertOne${name}(
+  where: ${name}WhereUniqueInput!
+  create: ${name}CreateInput!
+  update: ${name}UpdateInput!
+): ${name}Response!`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}Response)
-    async upsertOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.upsert({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }));
-    }`;
-  }
-
-  if (!exclude.includes('deleteMany')) {
-    operations.mutations.type += `
-    deleteMany${name}(where: ${name}WhereInput!): ${name}BatchResponse`;
+@Mutation((returns)=>${name}Response)
+async upsertOne${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}Response|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.upsert({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }));
+}`;
+}
+if (!exclude.includes('deleteMany')) {
+    mutations += `
+deleteMany${name}(where: ${name}WhereInput!): ${name}BatchResponse`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}BatchResponse)
-   async deleteMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}BatchResponse|any>{
-      ${onDelete
+@Mutation((returns)=>${name}BatchResponse)
+async deleteMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}BatchResponse|any>{
+  ${onDelete
         ? `await ctx.prisma.onDelete({ model: '${name}', where: args.where })`
-        : ''
-      }
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+        : ''}
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
 
-      return ctx.prisma.${model}.deleteMany({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }));
-    }`;
-  }
-
-  if (!exclude.includes('updateMany')) {
-    operations.mutations.type += `
-    updateMany${name}(
-      where: ${name}WhereInput!
-      data: ${name}UpdateManyMutationInput!
-    ): ${name}BatchResponse!`;
+  return ctx.prisma.${model}.deleteMany({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }));
+}`;
+}
+if (!exclude.includes('updateMany')) {
+    mutations += `
+updateMany${name}(
+  where: ${name}WhereInput!
+  data: ${name}UpdateManyMutationInput!
+): ${name}BatchResponse!`;
     operations.mutations.resolver += `
-    @Mutation((returns)=>${name}BatchResponse)
-    updateMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}BatchResponse|any>{
-      const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
-      return ctx.prisma.${model}.updateMany({...args,...select})
-      .then((data)=>({
-        status:true,
-        data,
-        message:'ok'
-      })).catch(({message})=>({
-       status:false,
-       data:null,
-       message:message||'unknown error'
-      }));
-    }`;
-  }
-
-  operations.queries.type += `
-  }`;
-  //    operations.queries.resolver += `
-  //  },`;
-  operations.mutations.type += `
-  }`;
-  //   operations.mutations.resolver += `
-  // }`;
+@Mutation((returns)=>${name}BatchResponse)
+updateMany${name}(@Parent() parent, @Args() args, @Context() ctx: TenantContext, @Info() info):Promise<${name}BatchResponse|any>{
+  const select = ctx.prisma.getSelection(info).valueOf('data', '${name}', { select: {  } });
+  return ctx.prisma.${model}.updateMany({...args,...select})
+  .then((data)=>({
+    status:true,
+    data,
+    message:'ok'
+  })).catch(({message})=>({
+   status:false,
+   data:null,
+   message:message||'unknown error'
+  }));
+}`;
+}
+if(queries.length>1){
+operations.queries.type = `
+extend type Query {
+${queries}
+}`;
+}
+if(mutations.length>1){
+operations.mutations.type = `
+extend type Mutation {
+${mutations}
+}`;
+}  
+return operations;
   return operations;
 }
